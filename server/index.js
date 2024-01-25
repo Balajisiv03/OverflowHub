5603393903eea5eb6c251f098852c291fac938e8
Balaji <balajisivakumar2003@gmail.com>
2024-01-25 22:57:28 +0530

    sec changes

--- server/index.js
+++ server/index.js
@@ -4,8 +4,10 @@ import cors from "cors"
 import userRoutes from './routes/users.js';
 import questionRoutes from './routes/Questions.js';
 import answerRoutes from './routes/Answers.js';
+import dotenv from 'dotenv';
 
 const app=express();
+dotenv.config();
 app.use(express.json({limit: "30mb",extended: true}))
 app.use(express.urlencoded({limit: "30mb",extended: true}))
 app.use(cors());
@@ -21,8 +23,7 @@ app.get("/",(req,res)=>{
 
 const PORT = process.env.PORT || 5000
 
-// const CONNECTION_URL="mongodb+srv://admin:admin@stack-overflow-clone.fgqn0.mongodb.net/?retryWrites=true&w=majority";
-const CONNECTION_URL="mongodb+srv://admin:admin@stack-overflow-clone.fgqn0.mongodb.net/";
-mongoose.connect(CONNECTION_URL,{useNewUrlParser: true,useUnifiedTopology: true})
+const DATABASE_URL=process.env.CONNECTION_URL
+mongoose.connect(DATABASE_URL,{useNewUrlParser: true,useUnifiedTopology: true})
    .then(()=>app.listen(PORT,()=>{console.log(`server is running on port ${PORT}`)}))
    .catch((err)=>console.log(err.message));
